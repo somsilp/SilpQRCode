ab2a3cb03d12ad91473856b49ce7b7ab771c7b0d
somsilp <somsilp@gmail.com>
2018-03-07 11:32:07 +0700

    Initial commit

---
+++ app/src/main/java/somsilp/cpn/co/th/silpqrcode/MainActivity.java
@@ -0,0 +1,13 @@
+package somsilp.cpn.co.th.silpqrcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
