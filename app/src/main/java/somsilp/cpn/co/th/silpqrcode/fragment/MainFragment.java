4ce8e6b9af9d732cf94efbd0aa993ba02ca763a1
somsilp <somsilp@gmail.com>
2018-03-07 15:34:20 +0700

    Create MainFragment Success

---
+++ app/src/main/java/somsilp/cpn/co/th/silpqrcode/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package somsilp.cpn.co.th.silpqrcode.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import somsilp.cpn.co.th.silpqrcode.R;
+
+/**
+ * Created by chsomsilp on 3/7/2018.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
