5e0150c76f1b79939461e1dc0e9149a6718a0f84
somsilp <somsilp@gmail.com>
2018-03-08 09:43:08 +0700

    Replace Fragment

--- app/src/main/java/somsilp/cpn/co/th/silpqrcode/fragment/MainFragment.java
+++ app/src/main/java/somsilp/cpn/co/th/silpqrcode/fragment/MainFragment.java
@@ -1,11 +1,13 @@
 package somsilp.cpn.co.th.silpqrcode.fragment;
 
+import android.icu.text.Replaceable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import somsilp.cpn.co.th.silpqrcode.R;
 
@@ -14,6 +16,33 @@ import somsilp.cpn.co.th.silpqrcode.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+        registerController();
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
+    }
 
     @Nullable
     @Override
