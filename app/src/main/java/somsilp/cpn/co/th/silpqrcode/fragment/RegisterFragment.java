e9e819eb8ff26bba14c0d27c55ee3f70730da1ea
somsilp <somsilp@gmail.com>
2018-03-08 10:35:31 +0700

    popToBack stack Success

--- app/src/main/java/somsilp/cpn/co/th/silpqrcode/fragment/RegisterFragment.java
+++ app/src/main/java/somsilp/cpn/co/th/silpqrcode/fragment/RegisterFragment.java
@@ -3,10 +3,12 @@ package somsilp.cpn.co.th.silpqrcode.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import somsilp.cpn.co.th.silpqrcode.MainActivity;
 import somsilp.cpn.co.th.silpqrcode.R;
 
 /**
@@ -14,6 +16,40 @@ import somsilp.cpn.co.th.silpqrcode.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+
+    }   // Main Method
+
+    private void createToolbar() {
+
+//        Config Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbatRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        Setup Title and Subtitle
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_register));
+
+//        Setup Navigator
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
